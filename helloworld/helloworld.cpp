f60500bed4e24bb8b15e33592cf623e8bf7691ad
gujinfei <gjfly007@163.com>
2019-11-10 15:58:22 +0800

    "vscode hello world compiled by lldb"

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,27 @@
+#include <event.h>
+#include <stdio.h>
+#include <time.h>
+
+static void
+hello(int fd, short event, void *arg)
+{
+    printf("hello man\n");
+}
+
+int main(int argc, const char *argv[]) 
+{
+    struct event_base *base;
+    struct timeval tm;
+    struct event evt;
+
+    evutil_timerclear(&tm);
+    tm.tv_sec = 10;
+
+    base = event_base_new();
+    //evtimer_set(&evt, hello, NULL);
+    event_set(&evt, -1, EV_PERSIST, hello, NULL);
+    event_base_set(base, &evt);
+    evtimer_add(&evt, &tm);
+    event_base_loop(base, 0);
+    return 0;
+}
